7802b05a71f86879d37347d6b9d75d6e972ce28e
MohammadReza PourMohammad <mrpm.root@gmail.com>
2026-05-15 20:54:15 +0330

    feat: add language support (en, fa)

---
+++ lang/en/local_bbb.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'local_bbb', language 'en'.
+ *
+ * @package     local_bbb
+ * @author      MohammadReza PourMohammad <onbirdev@gmail.com>
+ * @copyright   2026 MohammadReza PourMohammad
+ * @link        https://onbir.dev
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['active'] = 'Active';
+$string['analytics'] = 'Analytics';
+$string['attendance'] = 'Attendance';
+$string['bbb:manage'] = 'Manage BigBlueButton integration';
+$string['bbb:view_dashboard'] = 'View BigBlueButton learning dashboard';
+$string['bbb:view_reports'] = 'View BigBlueButton reports';
+$string['created_at'] = 'Created at';
+$string['dashboard'] = 'Dashboard';
+$string['error_api'] = 'BigBlueButton API error';
+$string['error_connection'] = 'Connection error';
+$string['error_dashboard_unavailable'] = 'Learning dashboard is unavailable';
+$string['error_invalid_sessiontoken'] = 'Invalid session token';
+$string['inactive'] = 'Inactive';
+$string['meeting'] = 'Meeting';
+$string['meeting_id'] = 'Meeting ID';
+$string['meeting_name'] = 'Meeting name';
+$string['no_permissions'] = 'You do not have permission to access this page.';
+$string['open_dashboard'] = 'Open learning dashboard';
+$string['pluginname'] = 'BigBlueButton integration';
+$string['privacy:metadata'] = 'The local_bbb plugin stores BigBlueButton meeting and analytics data.';
+$string['recordings'] = 'Recordings';
+$string['reports'] = 'Reports';
+$string['server'] = 'Server';
+$string['server_name'] = 'Server name';
+$string['server_url'] = 'Server URL';
+$string['session_token'] = 'Session token';
+$string['sessions'] = 'Sessions';
+$string['settings'] = 'Settings';
+$string['shared_secret'] = 'Shared secret';
+$string['status'] = 'Status';
+$string['task_cleanup'] = 'Cleanup old BigBlueButton data';
+$string['task_sync'] = 'Synchronize BigBlueButton data';
+$string['updated_at'] = 'Updated at';
---
+++ lang/fa/local_bbb.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'local_bbb', language 'fa'.
+ *
+ * @package     local_bbb
+ * @author      MohammadReza PourMohammad <onbirdev@gmail.com>
+ * @copyright   2026 MohammadReza PourMohammad
+ * @link        https://onbir.dev
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['active'] = 'فعال';
+$string['analytics'] = 'تحلیل‌ها';
+$string['attendance'] = 'حضور و غیاب';
+$string['bbb:manage'] = 'مدیریت یکپارچه‌سازی بیگ‌بلو‌باتن';
+$string['bbb:view_dashboard'] = 'مشاهده داشبورد یادگیری بیگ‌بلو‌باتن';
+$string['bbb:view_reports'] = 'مشاهده گزارش‌های بیگ‌بلو‌باتن';
+$string['created_at'] = 'تاریخ ایجاد';
+$string['dashboard'] = 'داشبورد';
+$string['error_api'] = 'خطا در API بیگ‌بلو‌باتن';
+$string['error_connection'] = 'خطا در اتصال';
+$string['error_dashboard_unavailable'] = 'داشبورد یادگیری در دسترس نیست';
+$string['error_invalid_session_token'] = 'توکن نشست نامعتبر است';
+$string['error_no_permissions'] = 'شما اجازه دسترسی به این صفحه را ندارید.';
+$string['inactive'] = 'غیرفعال';
+$string['meeting'] = 'جلسه';
+$string['meeting_id'] = 'شناسه جلسه';
+$string['meeting_name'] = 'نام جلسه';
+$string['open_dashboard'] = 'باز کردن داشبورد یادگیری';
+$string['pluginname'] = 'یکپارچه‌سازی بیگ‌بلو‌باتن';
+$string['privacy:metadata'] = 'پلاگین local_bbb اطلاعات جلسات و تحلیل‌های بیگ‌بلو‌باتن را ذخیره می‌کند.';
+$string['recordings'] = 'ضبط‌ها';
+$string['reports'] = 'گزارش‌ها';
+$string['server'] = 'سرور';
+$string['server_name'] = 'نام سرور';
+$string['server_url'] = 'آدرس سرور';
+$string['session_token'] = 'توکن نشست';
+$string['sessions'] = 'جلسات';
+$string['settings'] = 'تنظیمات';
+$string['shared_secret'] = 'کلید اشتراکی';
+$string['status'] = 'وضعیت';
+$string['task_cleanup'] = 'پاک‌سازی داده‌های قدیمی بیگ‌بلو‌باتن';
+$string['task_sync'] = 'همگام‌سازی داده‌های بیگ‌بلو‌باتن';
+$string['updated_at'] = 'تاریخ بروزرسانی';
